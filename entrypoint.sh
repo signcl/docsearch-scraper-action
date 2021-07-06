5d32f913bd70e3aedc8ee9597cab906cb590d37a
Tunghsiao Liu <t@sparanoid.com>
2021-07-06 04:43:36 -0400

    chore: add debug code

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
+sh -c "ls -lah"
+sh -c "ls -lah src"
 sh -c "pipenv run python -m src.index $*"
