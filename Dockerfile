77bb9c27f468e213a39d32c1b3799af26391dfbe
Tunghsiao Liu <t@sparanoid.com>
2021-07-06 05:17:38 -0400

    chore:  try /root as base dir

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
 FROM algolia/docsearch-scraper:latest
+
 LABEL maintainer="t@sparanoid.com"
+
+# Get rid of /github/home
+# https://stackoverflow.com/a/63144407/412385
+ENV WORKON_HOME /root
+ENV PIPENV_PIPFILE /root/Pipfile
+
+RUN pipenv install
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions
 
+cd /root
+
+ls -lah
+ls -lah src
+
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
-sh -c "ls -lah"
-sh -c "ls -lah src"
 sh -c "pipenv run python -m src.index $*"
